64a6ee22ff7a3944ea606cf56b75394625b54a58
dsmjoy <justredmmm@outlook.com>
2018-02-18 15:48:34 +0800

    add css reset

--- src/entries/vendors.js
+++ src/entries/vendors.js
@@ -3,3 +3,6 @@ import Vuex from 'vuex'
 import Router from 'vue-router'
 
 Vue.config.devtools = process.env.NODE_ENV === 'development'
+
+import { remAdjust } from '@/utils/index'
+remAdjust()
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,13 @@
+export function remAdjust () {
+    var Width = window.screen.width
+    var Height = window.screen.height
+    var scaleRatio = Width / 375
+    if( scaleRatio > 1.36 ) {
+      scaleRatio = 1.36
+    }
+    document.querySelector('html').style.fontSize = 100 * scaleRatio + 'px'
+}
 
+export default {
+    remAdjust
+}
