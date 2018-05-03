fed94a2435bc56ff9e05e6e9f44a9896694a957d
dsmjoy <justredmmm@outlook.com>
2018-05-03 19:16:02 +0800

    remove http floder

--- src/entries/vendors.js
+++ src/entries/vendors.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Router from 'vue-router'
 import '@/config/index'
-
 Vue.config.devtools = process.env.NODE_ENV === 'development'
-
 import { remAdjust } from '@/utils/index'
 remAdjust()
